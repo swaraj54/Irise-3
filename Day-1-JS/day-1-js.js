e405b07aff395a28e61f44dfcef66cfe4382a5fa
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-10-04 09:31:20 +0530

    Updates

---
+++ Day-1-JS/day-1-js.js
@@ -0,0 +1,56 @@
+// Datattypes  
+
+
+// String 
+// syntax ->    "" ,'' , ``
+
+console.log(typeof ("kuchhbhi  bolna hai"));
+console.log(typeof ('testing single coats'))
+console.log(typeof (`back ticks`))
+console.log(typeof ("12345"))
+console.log(typeof ("true"))
+
+
+// Number 
+
+// stytax -> 8976543
+
+console.log(typeof(2345678))
+
+
+// Boolean 
+
+// syntax -> true / false
+
+console.log(typeof (true))
+
+
+
+
+// Varibles 
+
+// var
+// let 
+// const 
+
+var swaraj = 987654
+console.log(swaraj)
+console.log(typeof (swaraj))
+
+var irise = "Hello students!"
+console.log(irise)
+console.log(typeof (irise))
+
+var isStudentCompletedAssignment = false; // true / false
+console.log(isStudentCompletedAssignment)
+console.log(typeof (isStudentCompletedAssignment))
+
+
+// Operations 
+
+var number1 = 23;
+var number2 = 98;
+var addition = number1 + number2;
+console.log(addition, "- addition")
+
+// sub , multi, divid
